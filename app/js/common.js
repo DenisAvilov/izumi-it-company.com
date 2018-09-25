f12ae526ded47a846ce498001983a6ff09b4dbf9
DenisAvilov <avilovd.a@gmail.com>
2018-09-25 03:19:40 +0300

    Top slider

--- app/js/common.js
+++ app/js/common.js
@@ -1,5 +1,22 @@
 $(document).ready(function(){
-    // $('.top-slider').owlCarousel();
+    $('.top-slider').owlCarousel({
+        loop:true,
+
+        nav:false,
+        dots:false,
+        itemClass : "top-wrap",
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:3
+            },
+            1000:{
+                items:1
+            }
+        }
+    });
 
 
 
